22c6ae47ef2aff10d3b5a52c4451333c5c3f7d77
hungkm <hungkm@hungkm-H61M-D2P-B3.(none)>
2013-08-29 17:42:26 +0700

    initialize devise gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mar::Application.config.secret_token = 'ca8e0558dfdb3fd48f6d94fed8743bfd5b2e6f002bf04c31a137c7512a11a132a55a63de27cbaa0008b357e772df648fe1409c49297a690a78e7857e38ad11ba'
